95cd13ae26d6c51cdc10ed325880daad556e6a8b
Hendrik Oosthuizen <thecodeblock.dev@gmail.com>
2024-12-26 16:19:39 +0200

    complete styling

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
 checkBtn.addEventListener("click", () => {
-    if (!input.value.trim()) return alert("Please input a value");
-    const userText = input.value;
-    const text = userText.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
-    const reversedText = text
-        .split("")
-        .reverse()
-        .join("");
-    result.textContent = text === reversedText ? `${userText} is a palindrome.` : `${userText} is not a palindrome.`;
+  if (!input.value.trim()) return alert("Please input a value");
+  const userText = input.value;
+  const text = userText.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  const reversedText = text.split("").reverse().join("");
+  result.textContent =
+    text === reversedText
+      ? `${userText} is a palindrome.`
+      : `${userText} is not a palindrome.`;
 });
